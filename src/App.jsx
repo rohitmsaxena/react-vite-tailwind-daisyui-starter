5a6a7ef5933e6817cb4efaf3f2ee9ddd11be7d1e
rohitsaxena <rohitmsaxena@gmail.com>
2025-02-03 17:19:21 -0700

    - added daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import daisyLogo from '/daisyui.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -8,17 +9,20 @@ function App() {
 
   return (
     <>
-      <div className="bg-gray-100">
+      <div className="bg-gray-100 flex items-center justify-center">
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo"/>
+        </a>
+        <a href="https://v5.daisyui.com/" target="_blank">
+          <img src={daisyLogo} className="logo daisy" alt="Daisyui logo"/>
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-5xl font-bold">Vite + React + DaisyUI</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="btn btn-neutral" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -26,7 +30,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite, React, and DaisyUI logos to learn more
       </p>
     </>
   )
